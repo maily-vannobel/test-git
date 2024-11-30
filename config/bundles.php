67e041c484d7d88d4aacd551ff1b092aa05b7750
maily-vannobel <maily.vannobel@laplateforme.io>
2024-11-30 19:23:25 +0100

    chore: remove all files to restart the project

--- config/bundles.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-
-];
